e9234a076b169dec20ba9ef9c7ea88e121dbff1c
Svetlana184 <sveta.kgd.39@gmail.com>
2026-09-16 16:32:18 +0200

    prototype lab

--- Labs/Lab_4/Program.cs
+++ Labs/Lab_4/Program.cs
@@ -3,12 +3,15 @@ originalCharacter.Name = "Артур";
 originalCharacter.Class = "Рыцарь";
 originalCharacter.Level = 10;
 originalCharacter.Health = 500;
+originalCharacter.Armor = 100;
 originalCharacter.Damage = 80;
 originalCharacter.Speed = 5;
 originalCharacter.Weapon = "Меч";
-Console.WriteLine("\nOriginal character:");
+Console.WriteLine("\n============================= Original character =============================\n");
 originalCharacter.ShowInfo();
 
+
+Console.WriteLine("\n============================= Copies =============================\n");
 Character newChar1 = originalCharacter.Clone();
 newChar1.Level = 10;
 newChar1.ShowInfo();
@@ -22,7 +25,89 @@ Character newChar3 = originalCharacter.Clone();
 newChar3.Level = 20;
 newChar3.ShowInfo();
 
+WarriorPrototype warriorPrototype = new WarriorPrototype();
+Character warriorChar = warriorPrototype.Clone();
+warriorChar.ShowInfo();
 
+Console.WriteLine("============================= СОЗДАНИЕ ПЕРСОНАЖА ============================= \n1 — Создать Воина \n2 — Создать Мага \n3 — Создать Лучника \n4 — Показать прототипы \n0 — Выход ");
+int charVar = int.Parse(Console.ReadLine()!);
+Character newChar = new Character();
+switch (charVar)
+{
+    case 1:
+        WarriorPrototype warriorPrototype1 = new WarriorPrototype();
+        newChar = warriorPrototype1.Clone();
+        newChar.ShowInfo();
+        break;
+    case 2:
+        WizardPrototype wizardPrototype = new WizardPrototype();
+        newChar = wizardPrototype.Clone();
+        newChar.ShowInfo();
+        break;
+    case 3:
+        ArcherPrototype archerPrototype = new ArcherPrototype();
+        newChar = archerPrototype.Clone();
+        newChar.ShowInfo();
+        break;
+    case 4:
+        WarriorPrototype warriorPrototype2 = new WarriorPrototype();
+        WizardPrototype wizardPrototype2 = new WizardPrototype();
+        ArcherPrototype archerPrototype2 = new ArcherPrototype();
+        Character w = warriorPrototype2.Clone();
+        w.ShowInfo();
+        Character wi = wizardPrototype2.Clone();
+        wi.ShowInfo();
+        Character ar = archerPrototype2.Clone();
+        ar.ShowInfo();
+        break;
+    default:
+        break;
+}
+
+class WarriorPrototype : Character
+{
+    public override Character Clone()
+    {
+        Character copyChar = new Character();
+        copyChar.Class = "warrior";
+        copyChar.Health = 500;
+        copyChar.Damage = 80;
+        copyChar.Speed = 5;
+        copyChar.Weapon = "Меч";
+        copyChar.Armor = 100;
+        return copyChar;
+    }
+}
+
+class WizardPrototype : Character
+{
+    public override Character Clone()
+    {
+        Character copyChar = new Character();
+        copyChar.Class = "wizard";
+        copyChar.Health = 250;
+        copyChar.Damage = 150;
+        copyChar.Speed = 7;
+        copyChar.Weapon = "Посох";
+        copyChar.Armor = 30;
+        return copyChar;
+    }
+}
+
+class ArcherPrototype : Character
+{
+    public override Character Clone()
+    {
+        Character copyChar = new Character();
+        copyChar.Class = "archer";
+        copyChar.Health = 300;
+        copyChar.Damage = 100;
+        copyChar.Speed = 10;
+        copyChar.Weapon = "Лук";
+        copyChar.Armor = 50;
+        return copyChar;
+    }
+}
 
 class Character
 {
@@ -31,6 +116,7 @@ class Character
     public int Level;
     public int Health;
     public int Damage;
+    public int Armor;
 
     public int Speed;
 
@@ -44,11 +130,12 @@ class Character
         Console.WriteLine($"Level: {this.Level}");
         Console.WriteLine($"Health: {this.Health}");
         Console.WriteLine($"Damage: {this.Damage}");
+        Console.WriteLine($"Armor: {this.Armor}");
         Console.WriteLine($"Speed: {this.Speed}");
         Console.WriteLine($"Weapon: {this.Weapon}\n");
     }
 
-    public Character Clone()
+    public virtual Character Clone()
     {
         Character copyChar = new Character();
         copyChar.Name = this.Name;
@@ -58,6 +145,7 @@ class Character
         copyChar.Damage = this.Damage;
         copyChar.Speed = this.Speed;
         copyChar.Weapon = this.Weapon;
+        copyChar.Armor = this.Armor;
         return copyChar;
     }
 }
